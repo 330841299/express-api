2b65452b62334f43f5aa8e72742d11c4917c4be7
330841299 <330841299@qq.com>
2016-09-27 15:46:48 +0800

    page

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var app = express();
 
-app.get('/song', function (req, res) {
-  console.log('song');
+app.get('/', function (req, res) {
+  var page = "<html>"+
+                "<body>"+
+                  "<h1>Index.html</h1>"+
+                "</body>"+
+            "</html>"
+  res.send(page)
 });
 
-app.get('/dapeng', function (req, res) {
-  console.log('dapeng');
+app.get('/song', function (req, res) {
+  var page = "<html>"+
+                "<body>"+
+                  "<h1>song.html</h1>"+
+                "</body>"+
+            "</html>"
+  res.send(page)
 });
 app.listen(4000,function(){
   console.log('runing on port 4000...');
